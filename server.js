2580980f2d14e6d6cc6d9139f9c041fbb9a55e32
Luis Peña <pezu.luis01@gmail.com>
2020-12-01 13:33:03 -0600

    ConflicMerge#2 Error DEVQA

--- server.js
+++ server.js
@@ -14,3 +14,7 @@
     console.log('Conflic-Merge#1: DEV-ANG'); // Conflic-Merge DEV-ANG
     console.log('Conflic-Merge#1: DEV-QA'); // Conflic-Merge DEV-QA
 /* ENDREGION: Dev-Ang */
+
+/* REGION: DEV QA */
+    console.log('Conflic-Merge#2: DEV-QA - ERROR'); // Conflic-Merge DEV-QA
+/* ENDREGION: DEV QA */
