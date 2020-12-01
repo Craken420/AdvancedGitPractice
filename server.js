25a98b77823a6dec20ec0483498f7a1f51ce5ea7
Luis Peña <pezu.luis01@gmail.com>
2020-12-01 11:19:56 -0600

    División por region de rama

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const app = express();
+/* REGION: Dev-Ang */
+    const express = require('express');
+    const app = express();
 
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send({ greet: 'Hi!, Welcome'})
-});
+    const port = 3000;
+    app.get('/', (req, res) => {
+        res.send({ greet: 'Hi!, Welcome'})
+    });
 
-const server = app.listen(port, ()=> {
-    console.log(`Server is running in http://localhost:${port}`);
-})
+    const server = app.listen(port, ()=> {
+        console.log(`Server is running in http://localhost:${port}`);
+    })
+/* ENDREGION: Dev-Ang */
