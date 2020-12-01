4edc5a8e13b4e5ae96be18f87efd80d5850e9dac
Luis Peña <pezu.luis01@gmail.com>
2020-12-01 11:11:18 -0600

    Corrección del Url y edicion del package

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 
 const port = 3000;
 app.get('/', (req, res) => {
-    res.send({ greet: 'Hi!, Welcome'});
+    res.send({ greet: 'Hi!, Welcome'})
 });
 
 const server = app.listen(port, ()=> {
-    console.log(`Server is running in https://localhost:${port}`);
+    console.log(`Server is running in http://localhost:${port}`);
 })
