4edf19e2935effb231180386c6ca3b036a44a9b4
Luis Peña <pezu.luis01@gmail.com>
2020-12-01 17:08:26 -0600

    Se agregaron los comentarios de la region All FROM DevQA
    Comentarios agregados, no codigo.

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-/* REGION Merge#0: DevANG */
-    const express = require('express');
-    const app = express();
+/* REGION All FROM DevQA: Rebase DevANG onto DevQA */
 
-    const port = 3000;
-    app.get('/', (req, res) => {
-        res.send({ greet: 'Hi!, Welcome'})
-    });
+    /* REGION Merge#0: DevANG */
+        const express = require('express');
+        const app = express();
 
-    const server = app.listen(port, ()=> {
-        console.log(`Server is running in http://localhost:${port}`);
-    })
-/* ENDREGION Merge#0: DevANG */
+        const port = 3000;
+        app.get('/', (req, res) => {
+            res.send({ greet: 'Hi!, Welcome'})
+        });
 
-/* REGION: DevQA Err */
-    console.log('ConflicMerge#2: CORRECCION__.DevANG -> DevQA'); // @OVERRIDE-ConflicMerge#2: DevANG 
-/* ENDREGION: DevQA Err */
+        const server = app.listen(port, ()=> {
+            console.log(`Server is running in http://localhost:${port}`);
+        })
+    /* ENDREGION Merge#0: DevANG */
 
-/* REGION: DevANG -> DevQA */
-    console.log('Merge#1: DevANG -> DevQA -> Main'); // @OVERRIDE-Merge#1: DevANG
+    /* REGION: DevQA Err */
+        console.log('ConflicMerge#2: CORRECCION__.DevANG -> DevQA'); // @OVERRIDE-ConflicMerge#2: DevANG 
+    /* ENDREGION: DevQA Err */
 
-    /* REGION: BOTH CHANGES */
-        console.log('ConflicMerge#1: DevANG -> DevQA'); // @OVERRIDE-ConflicMerge#1: DevANG
-        console.log('Rebase#1: DevANG -> DevQA'); // Rebase#1: DevQA
-    /* ENDREGION: BOTH CHANGES */
+    /* REGION: DevANG -> DevQA */
+        console.log('Merge#1: DevANG -> DevQA -> Main'); // @OVERRIDE-Merge#1: DevANG
 
-/* ENDREGION: DevANG -> DevQA */
+        /* REGION: BOTH CHANGES */
+            console.log('ConflicMerge#1: DevANG -> DevQA'); // @OVERRIDE-ConflicMerge#1: DevANG
+            console.log('Rebase#1: DevANG -> DevQA'); // Rebase#1: DevQA
+        /* ENDREGION: BOTH CHANGES */
 
+    /* ENDREGION: DevANG -> DevQA */
 
+/* ENDREGION All FROM DevQA: Rebase DevANG onto DevQA */
